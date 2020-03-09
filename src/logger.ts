1c92b1f5a91c5800f42fdad7f136ef5c8b223881
Jake 'Sid' Smith <jake@dabapps.com>
2020-03-09 22:27:26 +0000

    Improve logger

--- src/logger.ts
+++ src/logger.ts
@@ -1,4 +1,37 @@
-export const log = (message: string) => {
+import * as chalk from 'chalk';
+
+const getMessage = (message: unknown): string => {
+  if (typeof message === 'string' || typeof message === 'number') {
+    return message.toString();
+  }
+
+  if (message instanceof Error) {
+    return message.message || message.toString() || 'Unknown error';
+  }
+
+  try {
+    return JSON.stringify(message);
+  } catch {
+    return 'Unknown error';
+  }
+};
+
+const _log = (
+  message: string | number | Error,
+  color: 'white' | 'cyan' | 'red' | 'yellow'
+) => {
   // tslint:disable-next-line:no-console
-  console.log(message);
+  console.error(getMessage(chalk[color](message)));
+};
+
+export const log = (message: string | number | Error) => {
+  _log(message, 'white');
+};
+
+export const error = (message: string | number | Error, exit?: boolean) => {
+  _log(message, 'red');
+
+  if (exit) {
+    process.exit(1);
+  }
 };
